e49bae938e03642713b082b05e10c5f526d6a662
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2015-01-02 13:14:12 -0500

    dropped database and user migrations

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -6,4 +6,15 @@ class  MakersController < ApplicationController
   def new
     @maker = Maker.new
   end
+
+  # def create
+  #   @maker = Maker.new(maker_params)
+
+  #   if @maker.save
+  #     redirect_to @maker, notice: 'Maker was successfully created.'
+  #   else
+  #     flash[:notice] = @maker.errors.full_messages.join(". ")
+  #     render action: 'new'
+  #   end
+  # end
 end
